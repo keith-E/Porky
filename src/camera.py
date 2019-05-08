14fd5a317deb542875562d4062c01dd95e472707
Keith J Eagleson <keagleson28@gmail.com>
2019-05-08 10:19:21 -0400

    Update camera.py
    Cleaning.

--- src/camera.py
+++ src/camera.py
@@ -13,7 +13,8 @@ class Camera:
         while True:
             ret, frame = self.cap.read()
 
-            # Prepare input blob and perform an inference
+            # Put frames in the camera buffer.
+            # TODO: see if the empty check is removable
             if cam_buffer.empty():
                 cam_buffer.put(frame)
 
@@ -21,34 +22,36 @@ class Camera:
                 out = detection_buffer.get()
 
             if out is not None:
-                # Draw detected faces on the frame
-                for detection in out.reshape(-1, 7):
-                    confidence = float(detection[2])
-                    xmin = int(detection[3] * frame.shape[1])
-                    ymin = int(detection[4] * frame.shape[0])
-                    xy_min = (xmin, ymin)
-                    xmax = int(detection[5] * frame.shape[1])
-                    ymax = int(detection[6] * frame.shape[0])
-                    xy_max = (xmax, ymax)
-
-                    xmid = (xmax + xmin) // 2
-                    ymid = (ymax + ymin) // 2
-
-                    if confidence > 0.5:
-                        x = xmax - xmin
-                        y = ymax - ymin
-                        area = x * y
-                        area_buffer.put(area)
-                        center = (xmid, ymid)
-                        center_buffer.put(center)
-                        detection_details = [xy_min, xy_max, center, confidence]
-                        frame = image_overlay(frame, detection_details)
+                process_detection(frame, out)
 
             cv.imshow('frame', frame)
             if cv.waitKey(1) & 0xFF == ord('q'):
                 break
         self.cap.release()
         cv.destroyAllWindows()
+        
+    def process_detection(self, frame, out):
+        for detection in out.reshape(-1, 7):
+            confidence = float(detection[2])
+            xmin = int(detection[3] * frame.shape[1])
+            ymin = int(detection[4] * frame.shape[0])
+            xy_min = (xmin, ymin)
+            xmax = int(detection[5] * frame.shape[1])
+            ymax = int(detection[6] * frame.shape[0])
+            xy_max = (xmax, ymax)
+
+            xmid = (xmax + xmin) // 2
+            ymid = (ymax + ymin) // 2
+
+            if confidence > 0.5:
+                x = xmax - xmin
+                y = ymax - ymin
+                area = x * y
+                area_buffer.put(area)
+                center = (xmid, ymid)
+                center_buffer.put(center)
+                detection_details = [xy_min, xy_max, center, confidence]
+                frame = image_overlay(frame, detection_details)
 
 
 def image_overlay(image, details):
